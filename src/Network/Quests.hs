a012b541b1b6e880cbe1f7e4c265672309a11d3a
nuew <code@nuew.net>
2020-09-25 05:24:48 -0400

    Add a shared CSPRNG for the use of the Server.
    In the future, this could be upgraded to a RNG pool if demand makes that
    necessary. Additionally, if any further items are passed along with the
    Server, a containing structure should be used.
    
    Also do some prettification.

--- src/Network/Quests.hs
+++ src/Network/Quests.hs
@@ -2,19 +2,21 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 module Network.Quests
-        ( AppConfiguration(..)
-        , app
-        , applyMigrations
-        , dumpLayout
-        )
+  ( AppConfiguration(..)
+  , app
+  , applyMigrations
+  , dumpLayout
+  )
 where
 
 import           Control.Exception.Base
 import           Control.Monad.IO.Class
+import           Crypto.Random
 import           Data.Aeson
 import           Data.Aeson.TH
 import qualified Data.ByteString               as B
 import           Data.FileEmbed
+import           Data.IORef
 import           Data.Pool
 import qualified Data.Text                     as T
 import           Data.Time.Clock
@@ -35,49 +37,49 @@ data AppConfiguration = AppConfiguration
 
 migrations :: MigrationCommand
 migrations = MigrationCommands $ MigrationInitialization : migrationScripts
-    where
-        migrationDirectory = $(embedDir "./migrations/")
-        migrationScripts   = fmap (uncurry MigrationScript) migrationDirectory
+ where
+  migrationDirectory = $(embedDir "./migrations/")
+  migrationScripts   = fmap (uncurry MigrationScript) migrationDirectory
 
 doMigration :: MigrationCommand -> PG.Connection -> IO ()
 doMigration migrations conn = PG.withTransaction conn $ do
-        PG.execute_ conn "SET LOCAL client_min_messages = WARNING;"
-        runMigration ctx >>= migrateOrFail
-    where
-        ctx = MigrationContext { migrationContextCommand    = migrations
-                               , migrationContextVerbose    = False
-                               , migrationContextConnection = conn
-                               }
-        migrateOrFail f = case f of
-                MigrationSuccess -> return ()
-                MigrationError e -> errorWithoutStackTrace $ migrateErrMsg e
-        migrateErrMsg e = "Could not verify DB migrations (" ++ e ++ ")"
+  PG.execute_ conn "SET LOCAL client_min_messages = WARNING;"
+  runMigration ctx >>= migrateOrFail
+ where
+  ctx = MigrationContext { migrationContextCommand    = migrations
+                         , migrationContextVerbose    = False
+                         , migrationContextConnection = conn
+                         }
+  migrateOrFail f = case f of
+    MigrationSuccess -> return ()
+    MigrationError e -> errorWithoutStackTrace $ migrateErrMsg e
+  migrateErrMsg e = "Could not verify DB migrations (" ++ e ++ ")"
 
 setupDatabasePool :: AppConfiguration -> IO (Pool PG.Connection)
 setupDatabasePool cfg = do
-        pool <- createPool
-                (PG.connectPostgreSQL $ databaseConnection cfg)
-                PG.close
-                (databasePoolStripes cfg)
-                (databasePoolTimeout cfg)
-                (databasePoolMaxConns cfg)
-        withResource pool . doMigration $ MigrationValidation migrations
-        return pool
+  pool <- createPool (PG.connectPostgreSQL $ databaseConnection cfg)
+                     PG.close
+                     (databasePoolStripes cfg)
+                     (databasePoolTimeout cfg)
+                     (databasePoolMaxConns cfg)
+  withResource pool . doMigration $ MigrationValidation migrations
+  return pool
 
-server :: Pool PG.Connection -> Server ApiRoot
-server pool = return apiDocs :<|> apiV1Server pool
+server :: Pool PG.Connection -> IORef ChaChaDRG -> Server ApiRoot
+server pool rng = return apiDocs :<|> apiV1Server pool rng
 
 apiContext :: AppConfiguration -> Context '[JWTSettings, CookieSettings]
 apiContext cfg = jwtCtx :. defaultCookieSettings :. EmptyContext
-    where jwtCtx = defaultJWTSettings . fromSecret $ secretKey cfg
+  where jwtCtx = defaultJWTSettings . fromSecret $ secretKey cfg
 
 app :: AppConfiguration -> IO Application
 app cfg = bracket (setupDatabasePool cfg) destroyAllResources
-        $ \pool -> return $ serveWithContext api (apiContext cfg) (server pool)
+  $ \pool -> serveWithContext api (apiContext cfg) . server pool <$> setupRng
+  where setupRng = drgNew >>= newIORef
 
 applyMigrations :: AppConfiguration -> IO ()
 applyMigrations cfg = conn >>= doMigration migrations
-    where conn = PG.connectPostgreSQL $ databaseConnection cfg
+  where conn = PG.connectPostgreSQL $ databaseConnection cfg
 
 dumpLayout :: AppConfiguration -> String
 dumpLayout = T.unpack . layoutWithContext api . apiContext
--- src/Network/Quests/Server.hs
+++ src/Network/Quests/Server.hs
@@ -2,14 +2,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeOperators #-}
 module Network.Quests.Server
-        ( apiV1Server
-        )
+  ( apiV1Server
+  )
 where
 
 import           Data.Pool
+import           Crypto.Random
+import           Data.IORef
 import qualified Database.PostgreSQL.Simple    as PQ
 import           Network.Quests.API
 import           Servant
 
-apiV1Server :: Pool PQ.Connection -> Server ApiVersion1
+apiV1Server :: Pool PQ.Connection -> IORef ChaChaDRG -> Server ApiVersion1
 apiV1Server pool = errorWithoutStackTrace "unimplemented"
